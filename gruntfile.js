f4f1e5b534ef9e3114db8bcb83ed2b40943fef53
Gustavo Mariano <paulo.christiano@adm.unip.br>
2022-07-12 12:32:42 -0300

    merge conflict

--- gruntfile.js
+++ gruntfile.js
@@ -1,44 +1,44 @@
 module.exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks("grunt-contrib-concat");
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.initConfig({
-        concat: {
-            basic: {
-                src: [
-                    'assets/**/**/*.css',
-                    'assets/**/*.css'
-                ],
-                dest: 'temp/concat.css'
-            },
-            extras: {
-              src: [
-                    "assets/**/**/*.js",
-                    "assets/**/*.js"
-                  ],
-                  dest: "objetivo.js"
-            }
-        },
-        cssmin: {
-            options: {
-              mergeIntoShorthands: false,
-              roundingPrecision: -1
-            },
-            target: {
-              files: {
-                'objetivo.css': ['temp/concat.css']
-              }
-            }
-          },
-        watch: {
-            files: ['assets/**/**/*.css', 'assets/**/*.css', 'assets/**/**/*.js', 'assets/**/*.js'],
-            tasks: ['compile']
+  grunt.initConfig({
+    concat: {
+      basic: {
+        src: [
+          'assets/**/**/*.css',
+          'assets/**/*.css'
+        ],
+        dest: 'temp/concat.css'
+      },
+      extras: {
+        src: [
+          "assets/**/**/*.js",
+          "assets/**/*.js"
+        ],
+        dest: "objetivo.js"
+      }
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'objetivo.css': ['temp/concat.css']
         }
-    });
-    grunt.registerTask('compile', [
-        'concat',
-        'cssmin'
-    ]);
+      }
+    },
+    watch: {
+      files: ['assets/**/**/*.css', 'assets/**/*.css', 'assets/**/**/*.js', 'assets/**/*.js'],
+      tasks: ['compile']
+    }
+  });
+  grunt.registerTask('compile', [
+    'concat',
+    'cssmin'
+  ]);
 };
